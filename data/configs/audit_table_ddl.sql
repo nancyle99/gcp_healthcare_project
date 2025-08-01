299000669970e004348e30809de229c3b5911f6d
Nikhil Sharma <er.nikhilsharma7@gmail.com>
2025-08-01 23:10:33 +0530

    add data

---
+++ data/configs/audit_table_ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE active-district-466711-i0.temp_dataset.audit_log (
+    data_source STRING,
+    tablename STRING,
+    load_type STRING,
+    record_count INT64,
+    load_timestamp TIMESTAMP,
+    status STRING
+);
